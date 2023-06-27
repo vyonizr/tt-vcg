41519bb2d9dcf655b6ae9fc62ce226f6eef069b7
Fitrahtur Rahman <fitrahtur@gmail.com>
2023-06-27 08:57:58 +0700

    handle detail page when Pokemon ID is not found

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 import { getPokemonDetailURL } from '@/APIEndpoints'
+import { ERROR_MESSAGE } from '@/constants'
 import { IPokemonDetailAPIResponse } from '@/types'
 
 import PokemonDetailTableSkeleton from '@/components/PokemonDetailTableSkeleton'
@@ -15,18 +16,28 @@ export default function PokemonDetail() {
   const [pokemonDetail, setPokemonDetail] =
     useState<IPokemonDetailAPIResponse | null>(null)
   const [isDetailLoading, setIsDetailLoading] = useState(false)
-  const [isDetailError, setIsDetailError] = useState(false)
+  const [detailError, setDetailError] = useState<boolean | string>(false)
 
   useEffect(() => {
     async function fetchPokemonDetail(pokemonId: string) {
       try {
         setIsDetailLoading(true)
-        const res = await fetch(getPokemonDetailURL(pokemonId))
-        const pokemonDetail = await res.json()
+        const response = await fetch(getPokemonDetailURL(pokemonId))
+        if (response.status === 404) {
+          throw new Error(ERROR_MESSAGE.NOT_FOUND)
+        }
+        const pokemonDetail = await response.json()
         setPokemonDetail(pokemonDetail)
       } catch (error) {
-        console.error(error)
-        setIsDetailError(true)
+        if (
+          error instanceof Error &&
+          error.message === ERROR_MESSAGE.NOT_FOUND
+        ) {
+          setDetailError(ERROR_MESSAGE.NOT_FOUND)
+        } else {
+          console.error(error)
+          setDetailError(true)
+        }
       } finally {
         // intentionally created to simulate loading
         setTimeout(() => {
@@ -43,9 +54,13 @@ export default function PokemonDetail() {
   return (
     <>
       <Link href='/'>← Back to Home</Link>
-      {isDetailError ? (
+      {detailError ? (
         <div className='text-center'>
-          <h1 className='text-4xl'>An error occured</h1>
+          <h1 className='text-4xl'>
+            {detailError === ERROR_MESSAGE.NOT_FOUND
+              ? `Pokemon #${id} is not found`
+              : ERROR_MESSAGE.GENERAL}
+          </h1>
         </div>
       ) : isDetailLoading ? (
         <PokemonDetailTableSkeleton />
