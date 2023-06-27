c1059c1c5a89996dd9769f419845aa30632c2466
Fitrahtur Rahman <fitrahtur@gmail.com>
2023-06-27 07:39:02 +0700

    add dcoumentation link on RecoilObserver comp

--- src/components/RecoilObserver.tsx
+++ src/components/RecoilObserver.tsx
@@ -6,6 +6,7 @@ interface IProps {
   onChange: (value: any) => void
 }
 
+// https://recoiljs.org/docs/guides/testing/
 export default function RecoilObserver({ node, onChange }: IProps) {
   const value = useRecoilValue(node)
   useEffect(() => onChange(value), [onChange, value])
